22155a74d469550f50fa0019eea14d85a1de940c
tantry2002 <shree.tantry@gmail.com>
2015-06-06 16:25:06 +0530

    no

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('../..')(server);
+var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 server.listen(server_port,server_ip_address, function () {
-  console.log('Server listening at port %d', port);
+    console.log('Server listening at port %s:%d', server_ip_address, server_port);
 });
 
 // Routing
